8720cd7e71982d73cf257b100a9835845f30a4b0
balexand <alexandre.bel@ynov.com>
2024-04-12 13:42:30 +0200

    updated projects

--- src/app/components/about/page.tsx
+++ src/app/components/about/page.tsx
@@ -9,7 +9,8 @@ const About: React.FC<About> = ({fields, usedField}) => {
             {fields[usedField] == fields[0] && (
                 <div className="mx-24 mt-28 bg-black h-fit p-4 rounded-sm border border-black">
                   <p className="mb-12 text-6xl font-bold">BEL Alexandre</p>
-                  <p>I'm a 21-year-old web development enthusiast with a focus on full-stack development. Beyond crafting dynamic web experiences, I'm also versed in Python and Golang, tackling non-web projects with equal enthusiasm. I'm passionate about innovation, constantly pushing boundaries and embracing new challenges to refine my skills. Let's build something amazing together.</p>
+                  <p className="text-justify">I'm a 21-year-old web development enthusiast with a focus on full-stack development. Beyond crafting dynamic web experiences, I'm also versed in Python and Golang, tackling non-web projects with equal enthusiasm. I'm passionate about innovation, constantly pushing boundaries and embracing new challenges to refine my skills.</p>
+                  <p className="mt-10 font-bold">Let's build something together.</p>
                 </div>
               )}
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ import About from "./components/about/page";
 import Link from "next/link";
 
 export default function Home() {
-  const fields: string[] = ["ABOUT", "PROJECTS", "SKILLS", "CONTACT", "MATSUEL"];
-  const projectTitle: string[] = ["e-commerce", "ratio", "encore", "non man"];
-  const projectDesc: string[] = ["vente de fleurs", "vente d'organes", "vente d'enfants", "vraiment mais apagnan enfaite"];
-  const projectImg: string[] = ["https://pictures.tribuna.com/image/cfba80fb-24e6-4e1c-a911-57a8a0242358?width=1920&quality=70", "https://pictures.tribuna.com/image/cfba80fb-24e6-4e1c-a911-57a8a0242358?width=1920&quality=70", "https://pictures.tribuna.com/image/cfba80fb-24e6-4e1c-a911-57a8a0242358?width=1920&quality=70", "https://pictures.tribuna.com/image/cfba80fb-24e6-4e1c-a911-57a8a0242358?width=1920&quality=70"];
+  const fields: string[] = ["ABOUT", "PROJECTS", "SKILLS", "CONTACT"];
+  const projectTitle: string[] = ["e-commerce", "ratio", "encore", "non man", "ratio", "encore", "non man"];
+  const projectDesc: string[] = ["vente de fleurs", "vente d'organes", "vente d'enfants", "vraiment mais apagnan enfaite", "vente d'organes", "vente d'enfants", "vraiment mais apagnan enfaite"];
+  const projectImg: string[] = ["https://img.freepik.com/photos-gratuite/couleurs-vibrantes-nature-gros-plan-marguerite-violette-humide-generee-par-intelligence-artificielle_25030-63819.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1712793600&semt=ais", "https://img.freepik.com/photos-gratuite/couleurs-vibrantes-nature-gros-plan-marguerite-violette-humide-generee-par-intelligence-artificielle_25030-63819.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1712793600&semt=ais", "https://img.freepik.com/photos-gratuite/couleurs-vibrantes-nature-gros-plan-marguerite-violette-humide-generee-par-intelligence-artificielle_25030-63819.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1712793600&semt=ais", "https://img.freepik.com/photos-gratuite/couleurs-vibrantes-nature-gros-plan-marguerite-violette-humide-generee-par-intelligence-artificielle_25030-63819.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1712793600&semt=ais"];
   const [usedField, setUsedField] = useState<number>(0);
 
   const fieldFocus = (id: string) => {
@@ -20,7 +20,7 @@ export default function Home() {
 
   return (
     <div className="h-full flex font-mono text-white bg-black">
-      <nav className="w-1/5 h-screen bg-white">
+      <nav className="w-96 h-screen bg-white">
         <div className="flex items-center justify-center h-full flex-col text-xl bg-black">
           {fields.map((field: string, index: number) => (
             <div className="w-full z-40">
@@ -40,13 +40,14 @@ export default function Home() {
       <div className="w-4/5 h-full">
         <About fields={fields} usedField={usedField}/>
         {fields[usedField] == fields[1] && (
-            <div className="flex justify-center h-fit w-full px-28">
-              <div className="grid grid-cols-3 gap-10">
+            <div className="flex justify-center h-[800px] w-full px-28 overflow-y-auto">
+              <div className="grid grid-cols-3 gap-10 pt-2">
                 {projectTitle.map((title: string, index: number) => (
                   <Link href="https://github.com/flizze" target="-blank">
-                    <div className="border border-white p-3 text-2xl hover:-translate-y-2 transition duration-300 cursor-pointer hover:bg-white hover:text-black hover:border-black rounded-lg">
+                    <div className="border border-white p-3 text-2xl hover:-translate-y-2 transition duration-300 cursor-pointer hover:bg-white hover:text-black hover:border-black rounded-lg h-full">
                       <p className="font-fold mb-3">{title}</p>
                       <p>{projectDesc[index]}</p>
+                      <p>Php</p>
                       <img src={projectImg[index]}/>
                     </div>
                   </Link>
@@ -56,7 +57,7 @@ export default function Home() {
           )}
           {fields[usedField] == fields[4] && (
             <div className="flex justify-center h-full w-full items-center">
-              <img src="https://pictures.tribuna.com/image/cfba80fb-24e6-4e1c-a911-57a8a0242358?width=1920&quality=70" className="object-contain h-96 w-06"/>
+              <img src="https://img.freepik.com/photos-gratuite/couleurs-vibrantes-nature-gros-plan-marguerite-violette-humide-generee-par-intelligence-artificielle_25030-63819.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1712793600&semt=ais" className="object-contain h-96 w-96"/>
             </div>
           )}
         </div>
