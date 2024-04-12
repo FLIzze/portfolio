3b6dd67a45a3f06a594d36e3fe727d6f3223c773
flizze <alexandre.bel@hotmail.com>
2024-04-12 10:41:06 +0200

    front update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import About from "./components/about/page";
 
 export default function Home() {
-  const fields: string[] = ["ABOUT", "PROJECTS", "SKILLS", "CONTACT"];
+  const fields: string[] = ["ABOUT", "PROJECTS", "SKILLS", "CONTACT", "MATSUEL"];
   const [usedField, setUsedField] = useState<number>(0);
 
   const fieldFocus = (id: string) => {
@@ -15,25 +15,32 @@ export default function Home() {
 
 
   return (
-    <div className="h-full flex font-mono text-white bg-gradient-to-b to-green-300 from-[#88D498]">
-      <nav className="w-1/5 h-screen bg-white border-r border-black">
-        <div className="flex items-center justify-center h-full flex-col text-xl text-[#3E505B] z-10">
+    <div className="h-full flex font-mono text-white bg-black">
+      <nav className="w-1/5 h-screen bg-white">
+        <div className="flex items-center justify-center h-full flex-col text-xl bg-black">
           {fields.map((field: string, index: number) => (
-            <div className="w-full z-20">
+            <div className="w-full z-40">
                 {usedField !== index ? (
                   <div>
-                    <p className="text-white flex justify-end pr-6 text-2xl h-14 items-center bg-[#88D498] hover:bg-[#64a574] w-11/12 hover:w-full transition-all cursor-pointer border-b border-black border-r-2" id={index.toString()} onClick={() => fieldFocus(index.toString())}>{field}</p>
+                    <p className="pr-4 text-white flex justify-end text-2xl h-14 items-center bg-black hover:bg-white hover:text-black w-full cursor-pointer border-b border-white border-r-2 hover:w-[115%] transition-all duration-200 rounded-lg" id={index.toString()} onClick={() => fieldFocus(index.toString())}>{field}</p>
                   </div>
                 ) : (
                   <div>
-                    <p className="text-white flex justify-end pr-6 text-2xl h-14 items-center bg-[#64a574] w-11/12 transition-all border-b border-black border-r-2" id={index.toString()} onClick={() => fieldFocus(index.toString())}>{field}</p>
+                    <p className="flex justify-end pr-6 text-2xl h-14 items-center bg-white text-black border-b border-black border-r-2 rounded-lg" id={index.toString()} onClick={() => fieldFocus(index.toString())}>{field}</p>
                   </div>
                 )}
             </div>
           ))}
         </div>
       </nav>
+      <div className="w-full h-full">
       <About fields={fields} usedField={usedField} />
+        {fields[usedField] == fields[4] && (
+          <div className="flex justify-center h-full w-full items-center">
+            <img src="https://pictures.tribuna.com/image/cfba80fb-24e6-4e1c-a911-57a8a0242358?width=1920&quality=70" className="object-contain h-96 w-06"/>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
