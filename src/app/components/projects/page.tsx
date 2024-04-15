599190a2f0bd871f600dfeb5a63d0b5cd0a96ffa
flizze <alexandre.bel@hotmail.com>
2024-04-15 13:10:20 +0200

    navbar update

--- src/app/components/projects/page.tsx
+++ src/app/components/projects/page.tsx
@@ -1,88 +1,14 @@
 import Link from "next/link"
 
 interface Projects {
-    fields: string[];
-    usedField: number;
+    projects: string[];
+    usedProject: number;
 }
 
-interface Project {
-    title: string;
-    desc: string;
-    img: string;
-}
-
-const Projects: React.FC<Projects> = ({fields, usedField}) => {
-    const projects: Project[] = [
-        {
-            title: "forum",
-            desc: "Forum en golang",
-            img: "",
-        },
-        {
-            title: "Doftopia",
-            desc: "Dofus",
-            img: "",
-        },
-        {
-            title: "BlackJack Data",
-            desc: "Analyse de données sur le jeu du BlackJack",
-            img: "",
-        },
-        {
-            title: "E-commerce",
-            desc: "Site de vente de fleurs",
-            img: "",
-        },
-        {
-            title: "Impostor Bot",
-            desc: "Bot de l'imposteur en discord.",
-            img: "",
-        },
-        {
-            title: "Jeu 2d isometrique",
-            desc: "Typescript en canvas.",
-            img: "",
-        },
-        {
-            title: "Revenge on Inove",
-            desc: "Pareil mais python",
-            img: "",
-        },
-        {
-            title: "Portfolio",
-            desc: "Ca quoi",
-            img: "",
-        },
-        {
-            title: "Backup",
-            desc: "Systeme de backup en python",
-            img: "",
-        },
-        {
-            title: "ChatBox",
-            desc: "Systeme de chatbox en python",
-            img: "",
-        }
-    ]
-
+const Projects: React.FC<Projects> = ({projects, usedProject}) => {
     return (
         <div>
-            {fields[usedField] == fields[1] && (
-                <div className="flex justify-center h-[800px] w-full px-28 overflow-y-auto">
-                  <div className="grid grid-cols-3 gap-10 pt-2 h-fit">
-                    {projects.map((project: Project, index: number) => (
-                      <Link href="https://github.com/flizze" target="-blank" className="h-full"  key={index}>
-                        <div className="border border-white py-3 px-5 text-2xl hover:-translate-y-2 transition duration-300 cursor-pointer hover:bg-white hover:text-black hover:border-black rounded-lg h-full">
-                          <p className="font-fold mb-3">{project.title}</p>
-                          <p className="text-lg">{project.desc}</p>
-                          <p className="text-lg text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat odio esse exercitationem voluptatem consequatur saepe alias ab fugiat necessitatibus, nemo laborum accusamus debitis placeat aperiam dolores eos qui expedita corporis!</p>
-                          <img src={project.img}/>
-                        </div>
-                      </Link>
-                    ))}
-                  </div>
-                </div>
-              )}
+            <p className="text-4xl">{projects[usedProject]}</p>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,41 +3,64 @@
 import { useState } from "react";
 import About from "./components/about/page";
 import Projects from "./components/projects/page";
-import Skills from "./components/skills/page";
 
 export default function Home() {
-  const fields: string[] = ["ABOUT", "PROJECTS", "SKILLS", "CONTACT"];
+  const fields: string[] = ["A propos", "Projets", "Skills", "Contact"];
+  const projects: string[] = ["E-commerce", "Forum", "Doftopia", "Revenge on Inove", "BlackJack Data", "Impostor Bot", "Not named yet", "Portfolio"]
   const [usedField, setUsedField] = useState<number>(0);
+  const [usedProject, setUseProject] = useState<number>(0);
 
   const fieldFocus = (id: string) => {
-    const div = document.getElementById(id);
-    div!.style.background = '';
     setUsedField(parseInt(id));
   };
 
+  const projectFocus = (id: string) => {
+    setUseProject(parseInt(id));
+  }
+
   return (
     <div className="h-full flex font-mono text-white bg-black">
-      <nav className="w-96 h-screen bg-white">
+      <nav className="lg:w-80 w-52 h-screen bg-white transition-all duration-500">
         <div className="flex items-center justify-center h-full flex-col text-xl bg-black">
           {fields.map((field: string, index: number) => (
             <div className="w-full z-40" key={index}>
-                {usedField !== index ? (
+                {usedField != index ? (
                   <div>
-                    <p className="pr-4 text-white flex justify-end text-2xl h-14 items-center bg-black hover:bg-white hover:text-black w-full cursor-pointer border-b border-white border-r-2 hover:w-[115%] transition-all duration-200 rounded-r-lg" id={index.toString()} onClick={() => fieldFocus(index.toString())}>{field}</p>
+                      <p className="pr-4 text-white flex justify-end text-2xl h-14 items-center bg-black hover:bg-white hover:text-black w-full cursor-pointer border-b border-white border-r-2 hover:w-[115%] transition-all duration-200 rounded-r-lg" id={index.toString()} onClick={() => fieldFocus(index.toString())}>{field}</p>
                   </div>
                 ) : (
                   <div>
-                    <p className="flex justify-end pr-6 text-2xl h-14 items-center bg-white text-black border-b border-black border-r-2 rounded-r-lg transition-all" id={index.toString()} onClick={() => fieldFocus(index.toString())}>{field}</p>
+                    <p className="flex justify-end pr-4 text-2xl h-14 items-center bg-white text-black border-b border-black border-r-2 rounded-r-lg transition-all w-[115%]" id={index.toString()} onClick={() => fieldFocus(index.toString())}>{field}</p>
+                      {index == 1 && (
+                        <div className="transition-all">
+                          {projects.map((project: string, index: number) => (
+                            <div key={index} className="">
+                              {usedProject != index ?  (
+                                <div>
+                                  <p className="w-10/12 h-10 items-center border-white border-b border-r-2 hover:w-11/12 flex justify-end pr-4 cursor-pointer hover:bg-white rounded-r-lg hover:text-black transition-all" id={index.toString()} onClick={() => projectFocus(index.toString())}>{project}</p>
+                                </div>
+                              ): (
+                                <div>
+                                  <p className="h-10 items-center bg-white text-black w-11/12 flex justify-end pr-4 rounded-r-lg transition-all">{project}</p>
+                                </div>
+                              )}
+                            </div>
+                          ))}
+                        </div>
+                      )}
                   </div>
                 )}
             </div>
           ))}
         </div>
       </nav>
-      <div className="w-4/5 h-full">
+      <div className="w-4/5 h-screen">
+        {fields[usedField] == "Projets" && (
+          <div className="w-full flex justify-center h-full items-center">
+            <Projects projects={projects} usedProject={usedProject}/>
+          </div>
+        )}
         <About fields={fields} usedField={usedField}/>
-        <Projects fields={fields} usedField={usedField}/>
-        <Skills fields={fields} usedField={usedField}/>
         </div>
     </div>
   );
