3ec0728287c1a6e37565f7a5d979266e8faf59e9
balexand <alexandre.bel@ynov.com>
2024-04-21 13:32:45 +0200

    shortened 'fonctionnalites'

--- src/app/components/e-commerce/page.tsx
+++ src/app/components/e-commerce/page.tsx
@@ -6,16 +6,16 @@ const Ecommerce = () => {
                     <p className="my-5">Site d'e-commerce realise en <a className="text-green-400">Symfony</a> avec une base de donnee <a className="text-green-400">MySQL</a>.</p>
                     <h2 className="font-bold text-green-400 text-2xl">Fonctionnalités</h2>
                     <ul className="ml-12 list-disc text-justify">
-                        <li>Accueil: La page d'accueil vous permet d'explorer les arrangements floraux, les offres spéciales et les collections saisonnières.</li>
-                        <li>Marketplace: Explorez une variété de fleurs et de compositions florales dans notre marché virtuel, organisé en catégories pour correspondre à chaque occasion spéciale.</li>
-                        <li>Connexion/Création de compte: Les utilisateurs peuvent se connecter à leur compte existant pour gérer leurs préférences et suivre leurs commandes, ou créer un nouveau compte pour bénéficier d'avantages supplémentaires, comme la sauvegarde des informations de paiement et des adresses de livraison.</li>
-                        <li>Contactez-nous: Une équipe de service client est disponible pour répondre à vos questions et vous aider à choisir le bouquet parfait.</li>
-                        <li>Paramètres: Personnalisez votre expérience FLOWERS en ajustant vos préférences dans les paramètres du compte, y compris les informations personnelles, les notifications et le suivi des commandes.</li>
-                        <li>Tableau de bord: Accédez à un tableau de bord centralisé pour suivre toutes vos activités sur FLOWERS, y compris vos articles préférés, vos commandes en cours et plus encore.</li>
-                        <li>Articles: Parcourez une vaste gamme d'articles floraux disponibles à l'achat, chacun accompagné de descriptions détaillées et d'images.</li>
-                        <li>Mettre en Vente: Si vous êtes un créateur d'arrangements floraux, utilisez cette fonction pour partager vos créations avec la communauté FLOWERS, en créant des listes attrayantes avec des images, des descriptions et des prix.</li>
-                        <li>Avis et Commentaires: Partagez vos expériences d'achat en laissant des avis et lisez les commentaires des autres clients pour prendre des décisions éclairées.</li>
-                        <li>Panier: Ajoutez vos fleurs préférées à votre panier et suivez facilement vos sélections, avec la possibilité de créer plusieurs paniers pour différentes occasions.</li>
+                        <li>Accueil: Explorez arrangements floraux, offres spéciales et collections saisonnières.</li>
+                        <li>Marketplace: Découvrez fleurs et compositions florales par catégories pour chaque occasion.</li>
+                        <li>Connexion/Création de compte: Gérez préférences, commandes ou bénéficiez d'avantages en créant un compte.</li>
+                        <li>Contactez-nous: Équipe service client disponible pour vous aider à choisir le bouquet parfait.</li>
+                        <li>Paramètres: Personnalisez expérience FLOWERS en ajustant préférences, infos personnelles et notifications.</li>
+                        <li>Tableau de bord: Suivez activités sur FLOWERS, articles préférés, commandes en cours, etc.</li>
+                        <li>Articles: Parcourez gamme d'articles floraux avec descriptions détaillées et images.</li>
+                        <li>Mettre en Vente: Partagez vos créations florales avec la communauté FLOWERS.</li>
+                        <li>Avis et Commentaires: Partagez expériences d'achat et lisez avis des autres clients.</li>
+                        <li>Panier: Ajoutez vos fleurs préférées et suivez facilement vos sélections.</li>
                     </ul>   
                     <div>
                         <p className="text-green-400 w-full flex justify-center mb-4">[Home]</p>
