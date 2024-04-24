ac056435f733a3b06930e2b5894f280b62a40a22
flizze <alexandre.bel@hotmail.com>
2024-04-24 15:26:42 +0200

    revenge on inove fixed text size

--- src/app/components/revenge-on-inove/page.tsx
+++ src/app/components/revenge-on-inove/page.tsx
@@ -6,14 +6,14 @@ const RevengeOnInove = () => {
             <div className="lg:px-20 px-6 md:px-12 lg:h-screen h-full lg:overflow-y-auto mt-10 bg-[#272727] lg:bg-transparent md:text-xl text-base">
                 <h1 className="md:text-4xl text-3xl font-bold text-green-500 mb-6">Revenge on Inove</h1>
                 <p className="my-6">Bienvenue dans Revenge On Inove, un jeu d'aventure développé en <a className="text-bold text-green-500">Python</a> avec Pygame. Dans ce jeu, vous incarnez un étudiant astucieux qui a décidé de hacker son école. Cependant, cette action a rendu tous les étudiants, professeurs, mentors et membres de l'administration furieux contre vous. Pour regagner le contrôle, vous devez vous aventurer dans chaque étage du bâtiment et affronter vos adversaires.</p>
-                <h2 className="text-2xl text-green-500 font-bold">Controles</h2>
+                <h2 className="text-green-500 font-bold">Controles</h2>
                 <ul className="list-disc ml-10 mb-8">
                     <li>Utilisez les touches directionnelles pour vous déplacer dans le bâtiment et interargir avec les menus</li>
                     <li>Appuyez sur la touche d'espace pour courir.</li>
                     <li>Utiliser la touche e pour interagir.</li>
                     <li>Appuyez sur les chiffres 1-9 pour sélectionner un item ou utiliser l'ascenseur.</li>
                 </ul>
-                <h2 className="text-2xl text-green-500 font-bold">Déroulement du jeu</h2>
+                <h2 className="text-green-500 font-bold">Déroulement du jeu</h2>
                 <ul className="list-disc ml-10 mb-8">
                     <li>Exploration des étages</li>
                     <li>Affrontements épiques</li>
