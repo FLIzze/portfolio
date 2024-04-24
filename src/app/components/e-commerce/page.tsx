276f1efdad2773c237f2411fc6032d525de50f50
flizze <alexandre.bel@hotmail.com>
2024-04-24 14:42:46 +0200

    responsive update on text-size

--- src/app/components/e-commerce/page.tsx
+++ src/app/components/e-commerce/page.tsx
@@ -5,11 +5,11 @@ const Ecommerce = () => {
         <div>
             <div className="lg:px-20 px-6 md:px-12 lg:h-screen h-full lg:overflow-y-auto mt-10 bg-black lg:bg-transparent md:text-xl text-base">
                 <a href="https://flowers.alexandrebel.me" target="blank">
-                    <h1 className="text-4xl flex font-bold text-green-500 border-b border-green-500 w-fit hover:border-b-0">Flowers</h1>
+                    <h1 className="md:text-4xl text-3xl flex font-bold text-green-500 border-b border-green-500 w-fit hover:border-b-0">Flowers</h1>
                 </a>
                 <p className="my-6">Site d'e-commerce realise en <a className="text-green-500">Symfony</a> avec une base de donnee <a className="text-green-500">MySQL</a>.</p>
                 <h2 className="font-bold text-green-500">Fonctionnalités</h2>
-                <ul className="ml-10 list-disc text-justify mb-8">
+                <ul className="ml-10 list-disc lg:text-justify mb-8">
                     <li>Accueil: La page d'accueil vous permet d'explorer les arrangements floraux, les offres spéciales et les collections saisonnières.</li>
                     <li>Marketplace: Explorez une variété de fleurs et de compositions florales dans notre marché virtuel, organisé en catégories pour correspondre à chaque occasion spéciale.</li>
                     <li>Connexion/Création de compte: Les utilisateurs peuvent se connecter à leur compte existant pour gérer leurs préférences et suivre leurs commandes, ou créer un nouveau compte pour bénéficier d'avantages supplémentaires, comme la sauvegarde des informations de paiement et des adresses de livraison.</li>
--- src/app/components/forum/page.tsx
+++ src/app/components/forum/page.tsx
@@ -6,12 +6,12 @@ const Forum = () => {
         <div>
             <div className="lg:px-20 px-6 md:px-12 lg:h-screen h-full lg:overflow-y-auto mt-10 bg-black lg:bg-transparent md:text-xl text-base">
                 <a href="https://forum.alexandrebel.me" target="blank">
-                    <h1 className="text-4xl flex font-bold text-green-500 border-b border-green-500 w-fit hover:border-b-0">Forum</h1>
+                    <h1 className="md:text-4xl text-3xl flex font-bold text-green-500 border-b border-green-500 w-fit hover:border-b-0">Forum</h1>
                 </a>
-                <p className="my-6 text-xl">Forum entièrement fonctionnel. Realisé en <a className="text-green-500">golang</a> avec une base de données <a className="text-green-500">MySQL</a>.</p>
-                <div className="text-2xl">
+                <p className="my-6">Forum entièrement fonctionnel. Realisé en <a className="text-green-500">golang</a> avec une base de données <a className="text-green-500">MySQL</a>.</p>
+                <div>
                     <h2 className="font-bold text-green-500">Fonctionnalités</h2>
-                    <ul className="mb-8 text-xl list-disc ml-10">
+                    <ul className="mb-8 list-disc ml-10">
                         <li>Création d'un compte utilisateur</li>
                         <li>Création d'un nouveau sujet de discussion, ainsi que la possibilité d'ajouter des commentaires à ce sujet</li>
                         <li>Ajout de commentaires sur les sujets créés par d'autres utilisateurs</li>
@@ -21,7 +21,7 @@ const Forum = () => {
                         <li>Consultation du profil d'un autre utilisateur et envoi de messages privés</li>
                         <li>Modification et suppression des sujets ou des commentaires appartenant à l'utilisateur</li>
                     </ul>
-                    <div className="text-xl">
+                    <div>
                         <p className="text-green-500 w-full flex justify-center my-6">[Home]</p>
                         <a href="/forum/home.png" target="blank">
                             <Image src="/forum/home.png" draggable="false" alt="forum home" className="mb-6" width={1920} height={1080}/>
